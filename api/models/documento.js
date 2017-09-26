9865a4f094ed17c2915ca693ce4a5f167b3f7fe1
ealuna <edwlunan@gmail.com>
2017-09-26 08:33:49 -0500

    Modelos actualizados

--- api/models/documento.js
+++ api/models/documento.js
@@ -18,6 +18,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             primaryKey: true
         },
+
         serie: {
             type: DataTypes.INTEGER,
             primaryKey: true
